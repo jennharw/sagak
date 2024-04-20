35195fe19129fae5b9f3e71b4504180e01852c35
harim <holly001503@gmail.com>
2024-04-20 15:59:53 +0900

    refact: fix endpoint in urls.py

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -5,7 +5,7 @@ from .views import FoodViewSet, FoodSearchViewSet
 
 router = routers.DefaultRouter()
 router.register(r'food', FoodViewSet, 'food')
-router.register(r'food/search', FoodSearchViewSet, 'search')
+router.register(r'food-search', FoodSearchViewSet, 'search')
 
 urlpatterns = [
     path('', include(router.urls)),
